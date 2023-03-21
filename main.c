2bf53263ddbec5930e11ecb2039ec8bbbec905cc
Łukasz Kałużny <84326531+lukasz-kkk@users.noreply.github.com>
2023-03-21 18:46:00 +0100

    Update main.c

--- main.c
+++ main.c
@@ -5,9 +5,25 @@ int main() {
     char *filecontent = (char *)calloc(10240, 1);
 
     struct disk_t* disk = disk_open_from_file("example_fat12_volume.img");
+    if (disk == NULL){
+        disk_close(disk);
+        printf("Fat file could not be found.");
+        return 1;
+    }
     struct volume_t* volume = fat_open(disk, 0);
+    if(volume == NULL){
+        fat_close(volume);
+        disk_close(disk);
+        printf("Image file is not a valid FAT volume.");
+        return 2;
+    }
     struct file_t* file = file_open(volume, "MONEY");
-
+    if (file == NULL){
+        fat_close(volume);
+        disk_close(disk);
+        puts("The file does not exist.");
+        return 3;
+    }
     size_t size = file_read(filecontent, 1, 10240, file);
 
     printf("%s",filecontent);
